5800695314553ed275b38adc520e478082caaac0
kobraks <kobraks2@gmail.com>
2025-06-24 09:39:01 +0200

    Refactor Page class to use std::size_t
    Updated the Page class in the BeinzPlugin namespace to replace `size_t` with `std::size_t` for improved clarity and consistency. This change affects member variables, constructor parameters, and return types of accessors. The const qualifiers for member variables have been retained to prevent modification after initialization.

--- AreYouThereSE NG/src/Page/Page.h
+++ AreYouThereSE NG/src/Page/Page.h
@@ -6,21 +6,21 @@ namespace BeinzPlugin {
 		virtual ~Page() = default;
 		virtual RE::BSFixedString GenerateName() = 0;
 
-		size_t ID() const { return m_Page; }
-		size_t MaxItems() const { return m_MaxItems; }
+		std::size_t ID() const { return m_Page; }
+		std::size_t MaxItems() const { return m_MaxItems; }
 
 		bool IsEmpty() const { return m_Empty; }
 
 	protected:
-		Page(bool empty, size_t pageNumber, size_t maxItems);
+		Page(bool empty, std::size_t pageNumber, std::size_t maxItems);
 
 		void Empty(bool empty = true) { m_Empty = empty; }
 
 		RE::BSFixedString m_Name;
 
 	private:
-		const size_t m_MaxItems;
-		size_t m_Page;
+		const std::size_t m_MaxItems;
+		std::size_t m_Page;
 		bool m_Empty;
 	};
 }
